aa2fd30db6b9d4901eedf707f0f6dc8182559ef9
sltn-03 <achmadsultan2009@gmail.com>
2026-08-29 22:19:25 +0800

    update .sql - Sltn

--- config/pw-pos.sql
+++ config/pw-pos.sql
@@ -21,9 +21,7 @@ SET time_zone = "+00:00";
 -- Database: `pw-pos`
 --
 
-CREATE DATABASE IF NOT EXISTS pw-pos
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS pw-pos;
 
 USE pw-pos;
 
